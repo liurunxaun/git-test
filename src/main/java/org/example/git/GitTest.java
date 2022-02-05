9690e34e8acf383337457858ca541889d74a90be
liurunxuan <94226565+liurunxaun@users.noreply.github.com>
2022-02-05 10:47:04 +0800

    master 2 commit

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -3,5 +3,6 @@ package org.example.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 4");
+        System.out.println("master test");
     }
 }
